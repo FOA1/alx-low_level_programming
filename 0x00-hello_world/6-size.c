e985934aa750f632f74e26e5e4465ce014b4eac7
FOA1 <fataia500@gmail.com>
2022-09-08 20:30:14 +0000

    Size of var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,14 @@ int main(void)
 	int i;
 	double d;
 	char c;
-	printf("The size of an int is: %lu.\n", (unsigned long) sizeof(i));
-	printf("The size of an double is: %lu.\n", (unsigned long) sizeof(d));
-	printf("The size of a char is: %lu.\n", (unsigned long) sizeof(c));
+	float f;
+	long int li;
+	long long int lli;
+
+	printf("Size of a char is: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("Size of an int is: %lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("Size of a long int is: %lu byte(s)\n", (unsigned long) sizeof(li));
+	printf("Size of a long long int is: %lu byte(s)\n", (unsigned long) sizeof(lli));
+	printf("Size of a float is: %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
