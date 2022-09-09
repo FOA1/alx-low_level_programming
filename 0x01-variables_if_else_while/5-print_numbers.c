d1f2c924b12e748a9592693245529f2be346e74b
FOA1 <fataia500@gmail.com>
2022-09-09 22:29:48 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers
+ * starting from 0 in base 10
+ *
+ * Retuen: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	
+	printf("\n");
+
+	return (0);
+}
