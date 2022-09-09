a53ff5260ca9eee2a45ac950834df6d210a79596
FOA1 <fataia500@gmail.com>
2022-09-09 22:18:26 +0000

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter == 'q' || letter == 'e')
 			continue;
 		putchar(letter);
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
 
