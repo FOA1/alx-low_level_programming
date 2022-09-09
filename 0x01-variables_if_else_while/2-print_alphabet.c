ee6693fb7aeb3bf3e69e3157678c982e8ebdba0d
FOA1 <fataia500@gmail.com>
2022-09-09 21:43:03 +0000

    prints alphsbet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabet in lower case
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char alpha;
+	char letter;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++);
-	{
-		putchar(alpha);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
 	putchar('\n');
-	
-	return(0);
 
+	return (0);
 }
