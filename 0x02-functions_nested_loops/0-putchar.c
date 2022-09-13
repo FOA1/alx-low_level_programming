f9238d5df4de8f0301019a51e7d6d8a615584d6b
FOA1 <fataia500@gmail.com>
2022-09-13 15:04:21 -0700

    Fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - calls the function _putchar to 
+ * main - calls the function _putchar to
  * help us print instead of print f
  *
  * Return: Always 0.
@@ -9,7 +9,7 @@ int main(void)
 {
 	char myStr[] = "_putchar";
 	int i;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(myStr[i]);
