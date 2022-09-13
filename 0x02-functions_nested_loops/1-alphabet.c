12d100ade979f12fcdabbcbf744c7054c17f0804
FOA1 <fataia500@gmail.com>
2022-09-13 16:00:29 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Write a function that prints the alphabet,
+ * print_alphabet - Write a function that prints the alphabet,
  * in lowercase, followed by a new line.
  *
  * Return: Always 0.
  */
-int main(void)
+int main (void)
 {
 	int count;
 
@@ -15,5 +15,4 @@ int main(void)
 	}
 
 	_putchar('\n');
-	return (0);
 }
