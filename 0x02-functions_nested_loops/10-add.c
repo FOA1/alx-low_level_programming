a7e45b4d7ce6bf0b9fe2d2ad5149cf2df3e9350d
FOA1 <fataia500@gmail.com>
2022-09-14 09:48:02 -0700

    add sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Find the sum of two numbers
+ *
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: Always Success
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/test_1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	for (char c = '00'; c <= '23'; c++)
-		printf ("%s", c);
-
-	return (0);
-}
