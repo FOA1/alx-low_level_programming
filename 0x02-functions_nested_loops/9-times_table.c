f4f65c104f3b14ccc4e2a12e796a5493845554c1
FOA1 <fataia500@gmail.com>
2022-09-14 08:56:08 -0700

    Multiplication table 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - Write a function that prints multiplication
+ * table 9. starting from 0.
+ *
+ *
+ * Return: Always Success
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar((i * i) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
