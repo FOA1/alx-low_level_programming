9fdf779f8c16a5c2cc849019f2c1872fff41f599
FOA1 <fataia500@gmail.com>
2022-09-14 09:06:48 -0700

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 		{
 			product = i * j;
 			_putchar((product / 10) + '0');
-			_putchar((product % 10) = '0');
+			_putchar((product % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
