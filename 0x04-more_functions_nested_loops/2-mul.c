d214b0bbcc0b1ae498d56379746e6a347db39c59
FOA1 <fataia500@gmail.com>
2022-09-15 15:46:14 +0000

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Find the product of two numbers
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Always Success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
