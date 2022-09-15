4bb7158f96ab1d2615f42286382732a3bfecd4c0
FOA1 <fataia500@gmail.com>
2022-09-15 15:06:10 +0000

    Checks for digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isupper - Write a function that checks for lowercase.
+ * _isdigit - Write a function that checks for digits.
  * @c: The character to check
  *
  * Return: Always Success
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
