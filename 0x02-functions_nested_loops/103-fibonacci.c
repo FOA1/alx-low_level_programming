c4b647482c19a65955a88c59b4fd6a3a71b89822
FOA1 <fataia500@gmail.com>
2022-09-17 16:58:00 +0000

    More fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints Fibonacci sequence
+ * whose values do not exceed 4,000,000,
+ * write a program that finds and prints the sum of the even-valued terms,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	long int prev_num = 1,
+		pre_prev_num = 0,
+		new_num = 0,
+		sum = 0;
+
+	while (new_num <= 4000000)
+	{
+		new_num = prev_num + pre_prev_num;
+		pre_prev_num = prev_num;
+		prev_num = new_num;
+
+		if ((new_num % 2) == 0)
+		{
+			sum = sum + new_num;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
