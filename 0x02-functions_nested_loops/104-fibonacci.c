0f94f52bac7315084e91833d5a64c8e6bacec632
FOA1 <fataia500@gmail.com>
2022-09-17 17:25:46 +0000

    Fibonnaci to 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	long int prev_num = 1,
+		pre_prev_num = 0,
+		new_num = 0;
+
+	for (i = 1; i <= 98; i++)
+	{
+		new_num = prev_num + pre_prev_num;
+		pre_prev_num = prev_num;
+		prev_num = new_num;
+		if (i == 98)
+		{
+			printf("%ld", new_num);
+			continue;
+		}
+		printf("%ld, ", new_num);
+	}
+
+	printf("\n");
+	return (0);
+}
