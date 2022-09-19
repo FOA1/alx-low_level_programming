50e6f4344e544d1660326bcafda1019cd614470b
FOA1 <fataia500@gmail.com>
2022-09-19 13:22:41 +0000

    reseht to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value or the variable it points to to 98
+ *
+ * @n: The pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0. this resets the content of the address the ointer is pointing to to 98
+1. 
