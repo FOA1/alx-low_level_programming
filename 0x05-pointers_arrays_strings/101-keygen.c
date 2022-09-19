0f0dbd6b5f932e13041c064f9d6b118b35954752
FOA1 <fataia500@gmail.com>
2022-09-19 23:25:46 +0000

    random password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * randomPassword - generates random passwords for the user
+ *
+ * @n: the length of the password
+ */
+void randomPassword(int n)
+{
+	int i = 0;
+	int random = 0;
+
+	char num[] = "0123456789";
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbols[] = "!@$%^&*()_+=-?.,";
+	char password[n];
+
+	srand((unsigned int)(time(NULL)));
+	
+	random = rand() % 4;
+
+	for (i = 0; i < n; i++)
+	{
+		if (random == 1)
+		{
+			password[i] = num[rand() % 10];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (random == 2) 
+		{
+			password[i] = symbols[rand() % 8];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (random == 3)
+		{
+			password[i] = upper[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = lower[rand() % 26];
+			random = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+/**
+ * main _ the program that calls the function to
+ * generate the password
+ *
+ * Return: always 0.
+ */
+int main()
+{
+	int n = 8;
+
+	randomPassword(n);
+	printf("\n");
+	return 0;
+}
