1e4e5e369d815555cab5c1b2d2ba49945d1ab947
FOA1 <fataia500@gmail.com>
2022-09-19 21:26:01 +0000

    Print arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (a[0] == '\0')
-		printf("\n");
-	if (n == 1)
-		printf("%d\n", *a);
+	/* if ((a[0] == '\0') && (n < 1)) */
+	if (n <= 0)
+		printf('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
