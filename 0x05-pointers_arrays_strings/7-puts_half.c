7976e5a954cb3a188c8ff8815a9788356ff6d469
FOA1 <fataia500@gmail.com>
2022-09-19 19:35:38 +0000

    Prints half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *  puts_half - a function that prints the strings
+ *  in even number position,
+ *  @str: the string to print
+ */
+void puts_half(char *str)
+{
+	int l, i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+		l = i + 1;
+	if (l % 2 == 0)
+	{
+		for (i = l / 2; i < l; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else
+	{
+		n = (l - 1) / 2;
+		for (i = n + 1; i < l; i++)
+			_putchar(str[i]);
+		 _putchar('\n');
+	}
+}
