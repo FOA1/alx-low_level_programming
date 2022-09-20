2ea2cfde17cb4f407ae1fadd7c1bd6c085b6cc70
FOA1 <fataia500@gmail.com>
2022-09-20 17:54:40 +0000

    reverse the string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,20 @@ void print_rev(char *s)
 {
 	int l, i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		l = i + 1;
-
-	if (l == 0)
+	if (s[0] == '\0')
 		_putchar('\n');
 	else
 	{
-		for (i = l - 1; i >= 0; i--)
-			_putchar(s[i]);
-		_putchar('\n');
+		for (i = 0; s[i] != '\0'; i++)
+			l = i + 1;
+
+		if (l == 0)
+			_putchar('\n');
+		else
+		{
+			for (i = l - 1; i >= 0; i--)
+				_putchar(s[i]);
+			_putchar('\n');
+		}
 	}
 }
-
