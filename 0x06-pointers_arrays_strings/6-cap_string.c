c7ee94a4cff43d7b95a5a8be53f9c5fc2dc27fa2
FOA1 <fataia500@gmail.com>
2022-09-22 00:01:28 +0000

    Change  first letter of word to upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - converts strings from lower to upper case
+ *
+ * @p: the string to convert
+ *
+ * Return: the string p.
+ */
+char *cap_string(char *p)
+{
+	int i;
+	if (p[0] >= 'a' && p[0] <= 'z')
+		p[0] = p[0] - 32;
+
+	for (i = 1; p[i] != '\0'; i++)
+	{
+		if (p[i] == ' ' || p[i] == '.' || p[i] == ',' || p[i] == '\t' || p[i] == '\n')
+		{
+			if (p[i + 1] >= 'a' && p[i + 1] <= 'z')
+				p[i + 1] = p[i + 1] - 32;
+		}
+	}
+	return (p);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@
 	void reverse_array(int *a, int n);
 	void print_array(int *a, int n);
 	char *string_toupper(char *p);
+	char *cap_string(char *p);
 
 #endif /* MAIN-H */
