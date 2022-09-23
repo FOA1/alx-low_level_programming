5fb1a0937874a400c2d171e50522de933d48a7a2
FOA1 <fataia500@gmail.com>
2022-09-23 11:13:37 +0000

    String copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,24 +10,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, lenSrc = 0, lenDest = 0;
+	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		lenSrc = i + 1;
-	for (i = 0; dest[i] != '\0'; i++)
-		lenDest = i + 1;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	if (lenSrc < 0 || n < 0)
-		return (dest);
-	else
-	{
-		if (lenDest < n)
-			return (dest);
-		for (i = 0; i < n && src[i] != '\0'; i++)
-		{
-			dest[i] = src[i];
-		}
-	}
 	for (; i < n; i++)
 		dest[i] = '\0';
 
