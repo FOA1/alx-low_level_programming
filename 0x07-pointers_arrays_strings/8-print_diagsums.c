2762f2fbc5b461f26a852e1dbaaeea3854bae9af
FOA1 <fataia500@gmail.com>
2022-09-27 03:03:55 +0000

    fixed strchr

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,20 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum = 0;
+	int i, j, sum1, sum2;
+	int *p;
+
+	p = &a[i][j];
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (i == j || i + j == size)
-				sum = sum + a[i][j];
+			if (i == j)
+				sum1 = sum1 + *p[i][j];
+			if (i + j == size)
+				sum2 = sum2 + *(p + j);
 		}
 	}
-	printf("%d", sum);
+	printf("%d, %d\n", sum1, sum2);
 }
