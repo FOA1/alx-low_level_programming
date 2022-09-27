edf3ebca6c725f3bd778c3452c6d12755217b1eb
FOA1 <fataia500@gmail.com>
2022-09-27 05:52:03 +0000

    string break

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - gets lengthof a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: the first match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	/*unsigned int i = 0, j = 0;*/
+
+	for (; *s != '\0'; s++)
+	{
+		for (; *accept != '\0'; accept++)
+		{
+			if (s == accept)
+			{
+				return (s);
+			}
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@
 	void print_chessboard(char (*a)[8]);
 	int _putchar(char c);
 	unsigned int _strspn(char *s, char *accept);
+	char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN-H */
