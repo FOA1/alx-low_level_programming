d712afe782b33bc8db5709e34c372600389d8122
FOA1 <fataia500@gmail.com>
2022-09-27 09:05:27 +0000

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets string using double pointer
+ * @s: the double pointer
+ * @to: the main pointer
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@
 	int _putchar(char c);
 	unsigned int _strspn(char *s, char *accept);
 	char *_strpbrk(char *s, char *accept);
+	void set_string(char **s, char *to);
 
 #endif /* MAIN-H */
