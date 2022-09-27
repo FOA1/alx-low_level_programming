f12173014f61e1a050ff3ae0b7c8e101afbac2fe
FOA1 <fataia500@gmail.com>
2022-09-27 11:12:57 +0000

    Fixed task 2

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i= 0; j = 0;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (haystack = needle)
+
+	}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,16 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i = 0, j = 0, sum1 = 0, sum2 = 0;
+	int *p = (*a)[size];
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 = sum1 + *(a[i] + j);
+				sum1 = sum1 + *(*(a + i) + j);
 			if (i + j == size)
-				sum2 = sum2 + *(a[i] + j);
+				sum2 = sum2 + *(*(a + i) + j);
 		}
 	}
 	printf("%d, %d\n", sum1, sum2);
