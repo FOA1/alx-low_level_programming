090a26ce8dfe1e449eee15927f9f244810a24fa2
FOA1 <fataia500@gmail.com>
2022-09-27 15:44:19 +0000

    Strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int count = 0;
+	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-				count = count + 1;
+				break;
 		}
+		if (!accept[j])
+			break;
 	}
-	return (count);
+	return (i);
 }
