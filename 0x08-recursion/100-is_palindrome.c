9c09f70fee428ad18626e525ee5fad36596d718d
FOA1 <fataia500@gmail.com>
2022-09-29 04:40:38 +0000

    Palindrome checker

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * is_palindrome - checks a string if it is palindrome or not
+ *
+ * @s: the string to check
+ * Return: 1 if it s palindrome or 0 if it is not
+ */
+int is_palindrome(char *s)
+{
+	int flag = 1;
+
+	check(s, 0, _strlen_recursion(s) - 1, &flag);
+	return (flag);
+}
+/**
+ * check - checks if a string is palindrome or not
+ *
+ * @s: string to be checked
+ * @start: start index
+ * @end: end index
+ * @flag: flag to indicate if a string is a palindrome
+ *
+ * Return: void
+ */
+void  check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: the string to check
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+	if (!*s)
+	{
+		count++;
+		count += (_strlen_recursion(s + 1));
+	}
+	return (count);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@
 	int _pow_recursion(int x, int y);
 	int wildcmp(char *s1, char *s2);
 	int is_prime_number(int n);
-	 int is_prime(int n, int start);
+	int is_prime(int n, int start);
+	int is_palindrome(char *s);
+	void  check(char *s, int start, int end, int *flag);
 
 #endif /* MAIN_H */
