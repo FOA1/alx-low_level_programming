8c558dcc311ae9c2700d9a73d64ce396e0d5b18d
FOA1 <fataia500@gmail.com>
2022-10-04 12:32:16 +0000

    Malloc for 2-D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,12 +15,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	p = (int **) malloc(height * 4);
+	p = (int **) malloc(height * 8);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		p[i] = (int*)malloc(width * 4);
+		p[i] = (int *) malloc(width * 4);
 		if (p == NULL)
 			return (NULL);
 	}
