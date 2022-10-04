37f2bdab90bcb0a6ca2beb9bb490dae0e7cfe761
FOA1 <fataia500@gmail.com>
2022-10-04 11:04:19 +0000

    string Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,38 +12,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, len1 = 0, len2 = 0, totLen = 0;
 	char *p;
 
-	if (s1 != NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			len1++;
-	}
-	if (s2 != NULL)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-			len2++;
-	}
-
 	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	if (s1 != NULL && s2 == NULL)
 	{
-		p = (char *) malloc(len1 + 1);
-		if (p == NULL)
-			return (NULL);
-		for (i = 0; i < len1; i++)
-			*(p + i) = s1[i];
-		return (p);
+		s1 = "";
+		s2 = "";
 	}
-
-	if (s1 == NULL && s2 != NULL)
-	{
-		p = (char *) malloc(len2 + 1);
-		if (p == NULL)
-			return (NULL);
-		for (i = 0; i < len2; i++)
-			*(p + i) = s2[i];
-		return (p);
-	}
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
 
 	totLen = len1 + len2;
 	p = (char *) malloc(totLen + 1);
