7b46db645d2895ccab1d1afd68b09211c8bc876e
FOA1 <fataia500@gmail.com>
2022-10-04 08:57:59 +0000

    Malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *p;
 
 	p = (char *) malloc(size);
-	if (p == NULL)
+	if (p == NULL || size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
