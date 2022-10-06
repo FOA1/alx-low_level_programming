e5228f9852555fc874ab7f2f03cc61ec8a2c45aa
FOA1 <fataia500@gmail.com>
2022-10-06 21:05:29 +0000

    More Malloc snd free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else if (ptr != NULL && new_size == 0)
 		free(ptr);
 	else if (new_size == old_size)
-		return(ptr);
+		return (ptr);
 
-	return(p);
+	return (p);
 }
