09c66694253802e9d70186154df549e3c43c7d19
FOA1 <fataia500@gmail.com>
2022-10-06 21:32:15 +0000

    malloc Multiply

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints the arguments passed into it.
+ *
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int product = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+
+	return (0);
+}
+
