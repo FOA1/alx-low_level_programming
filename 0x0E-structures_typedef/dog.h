01c058d0fbd4779866d390577c3bb2e7fffd510f
FOA1 <fataia500@gmail.com>
2022-10-10 19:30:21 +0000

    Structures task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the  details of a dog
+ * @name: the name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Define a new type struct dog with the following elements:
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* Dog_H */
