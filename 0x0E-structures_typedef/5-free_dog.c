9abf3c14f6be49156e847cd35c7132cd3e2f17a5
FOA1 <fataia500@gmail.com>
2022-10-11 22:55:54 +0000

    Free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,18 @@
 #include "dog.h"
+#include <stdlib.h>
+
 /**
  * free_dog - free dog
  * @d: pointer to dog_t
+ *
+ * Return - Nothing
  */
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
 
