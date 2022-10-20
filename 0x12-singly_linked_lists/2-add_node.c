89ae46ba8b8587b418ff9dd5fe931ce4f018c52c
FOA1 <fataia500@gmail.com>
2022-10-20 21:26:48 +0000

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the beginning of a list
+ * @head: the head of the list
+ * @str: the string to be copied into the elements
+ *
+ * Return: the address of the newly created list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		printf("Error\n");
+
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
