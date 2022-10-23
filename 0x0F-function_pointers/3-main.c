e88a21cd90c86f6e0e5f79c2d5b7cc561b2e0264
FOA1 <fataia500@gmail.com>
2022-10-23 12:40:35 +0000

    Task 3 function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1])
+	if (argv[2][1] && argv[2][0] != ('+' || '-' || '*' || '/' || '%'))
 	{
 		printf("Error\n");
 		exit(99);
