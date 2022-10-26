861afa2cb0814a8e3dde7673e8ca26a0afce761f
FOA1 <fataia500@gmail.com>
2022-10-26 09:16:25 +0000

    More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int len = 0, i;
 
 	if (*head == NULL)
-		return (NULL);
+	{
+		newnode = malloc(sizeof(listint_t));
+		newnode->next = head;
+		head = newnode;
+		return (newnode);
+	}
 	temp = *head;
 	while (temp != NULL)
 	{
