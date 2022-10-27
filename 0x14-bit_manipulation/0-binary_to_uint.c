ea203a7a8166650ea7ce5ee62ca889c24946381e
FOA1 <fataia500@gmail.com>
2022-10-27 21:05:15 +0000

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,22 @@
 #include "main.h"
 /**
+ * _pow - finds the power of a number
  *
+ * @base: the base number
+ * @pow: the power
  *
- *
- *
- *
- *
- *
- *
+ * Return: the base raised to the power
  */
 int _pow(int base, int pow)
 {
 	int pro = 1;
 
-	while(pow > 0)
+	while (pow > 0)
 	{
 		pro = pro * base;
 		pow--;
 	}
-	return(pro);
+	return (pro);
 }
 
 
@@ -34,7 +32,7 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0, p = 0, num = 0, k = 0;
 
 	if (b == NULL)
-		return(0);
+		return (0);
 	while (b[i] != '\0')
 		i++;
 	i--;
@@ -42,10 +40,10 @@ unsigned int binary_to_uint(const char *b)
 	{
 		k = (b[i]) - '0';
 		if (k != 0 && k != 1)
-			return(0);
+			return (0);
 		num = num + (k * (_pow(2, p)));
 		p++;
 		i--;
 	}
-	return(num);
+	return (num);
 }
