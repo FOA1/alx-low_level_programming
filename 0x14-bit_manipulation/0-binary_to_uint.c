a6840d41d77935ba4953bde33d0b284b9783ea8d
FOA1 <fataia500@gmail.com>
2022-10-27 20:59:07 +0000

    Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int _pow(int base, int pow)
+{
+	int pro = 1;
+
+	while(pow > 0)
+	{
+		pro = pro * base;
+		pow--;
+	}
+	return(pro);
+}
+
+
+/**
+ * binary_to_uint - converts binary to integer
+ *
+ * @b: the number to convert
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, p = 0, num = 0, k = 0;
+
+	if (b == NULL)
+		return(0);
+	while (b[i] != '\0')
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		k = (b[i]) - '0';
+		if (k != 0 && k != 1)
+			return(0);
+		num = num + (k * (_pow(2, p)));
+		p++;
+		i--;
+	}
+	return(num);
+}
