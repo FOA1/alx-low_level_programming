4246524c586f380292482969a2d9682dacdbed95
FOA1 <fataia500@gmail.com>
2022-10-29 22:19:27 +0000

    function pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,28 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev - a function that prints a string in reversed order,
- * followed by a new line, to stdout
+ * print_rev - prints string in reverse
  *
- * @s: the string to print
+ * @s: the required string to print
+ * Return: Nothing
  */
 void print_rev(char *s)
 {
-	int l, i;
+	unsigned int len = 0;
 
-	if (s[0] == '\0')
-		_putchar('\n');
-	else
+	while (*s)
 	{
-		for (i = 0; s[i] != '\0'; i++)
-			l = i + 1;
-
-		if (l == 0)
-			_putchar('\n');
-		else
-		{
-			for (i = l - 1; i >= 0; i--)
-				_putchar(s[i]);
-			_putchar('\n');
-		}
+		len++;
+		s++;
 	}
+	s--;
+	while (len > 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/copy_4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string in reversed order,
+ * followed by a new line, to stdout
+ *
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+	int l, i;
+
+	if (s[0] == '\0')
+		_putchar('\n');
+	else
+	{
+		for (i = 0; s[i] != '\0'; i++)
+			l = i + 1;
+
+		if (l == 0)
+			_putchar('\n');
+		else
+		{
+			for (i = l - 1; i >= 0; i--)
+				_putchar(s[i]);
+			_putchar('\n');
+		}
+	}
+}
